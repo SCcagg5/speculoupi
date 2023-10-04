95cb73e62c9b174a5e1f6e208a71ca46333b85c5
boulinb <37933218+boulinb@users.noreply.github.com>
2023-10-04 23:25:55 +0200

    Configs with contractAddress

--- src/config/config.js
+++ src/config/config.js
@@ -1,55 +1,74 @@
 export default {
     polygon: {
+        chainId: 137,
+        jsonRpc: '',
         supportedAaveV3Tokens: [{
-            abbreviation: "AAVE"
+            abbreviation: "AAVE",
+            contactAddress: "0xd6df932a45c0f255f85145f286ea0b292b21c90b"
             },
             {
-                abbreviation: "BAL"
+                abbreviation: "BAL",
+                contactAddress: "0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3"
             },
             {
-                abbreviation: "CRV"
+                abbreviation: "CRV",
+                contactAddress: "0x172370d5Cd63279eFa6d502DAB29171933a610AF"
             },
             {
-                abbreviation: "DAI"
+                abbreviation: "DAI",
+                contactAddress: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
             },
             {
-                abbreviation: "DPI"
+                abbreviation: "DPI",
+                contactAddress: "0x85955046df4668e1dd369d2de9f3aeb98dd2a369"
             },
             {
-                abbreviation: "EURS"
+                abbreviation: "EURS",
+                contactAddress: "0xe111178a87a3bff0c8d18decba5798827539ae99"
             },
             {
-                abbreviation: "GHST"
+                abbreviation: "GHST",
+                contactAddress: "0x385eeac5cb85a38a9a07a70c73e0a3271cfb54a7"
             },
             {
-                abbreviation: "LINK"
+                abbreviation: "LINK",
+                contactAddress: "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39"
             },
             {
-                abbreviation: "SUSHI"
+                abbreviation: "SUSHI",
+                contactAddress: "0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a"
             },
             {
-                abbreviation: "USDC"
+                abbreviation: "USDC",
+                contactAddress: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"
             },
             {
-                abbreviation: "USDT"
+                abbreviation: "USDT",
+                contactAddress: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
             },
             {
-                abbreviation: "WETH"
+                abbreviation: "WETH",
+                contactAddress: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"
             },
             {
-                abbreviation: "WBTC"
+                abbreviation: "WBTC",
+                contactAddress: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6"
             },
             {
-                abbreviation: "WMATIC"
+                abbreviation: "WMATIC",
+                contactAddress: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"
             },
             {
-                abbreviation: "agEUR"
+                abbreviation: "agEUR",
+                contactAddress: "0xe0b52e49357fd4daf2c15e02058dce6bc0057db4"
             },
             {
-                abbreviation: "jEUR"
+                abbreviation: "jEUR",
+                contactAddress: "0x4e3decbb3645551b8a19f0ea1678079fcb33fb4c"
             },
             {
-                abbreviation: "miMATIC"
+                abbreviation: "miMATIC",
+                contactAddress: "0xa3fa99a148fa48d14ed51d610c367c61876997f1"
             }
         ]
     }
--- src/prices.js
+++ src/prices.js
@@ -1,3 +1,6 @@
+import config from "./config/config.js";
+
 export const getPrices = () => {
+    console.log(config);
     return [];
 }
