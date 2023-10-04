30ddf283a5d6a42fc827e33f256ac99ad4383c02
SCcagg5 <sccagg5@gmail.com>
2023-10-04 23:24:03 +0200

    Update roads.js

--- src/roads.js
+++ src/roads.js
@@ -1,3 +1,49 @@
-export const getRoads = () => {
-    return [];
+const { Fetcher, Token, TokenAmount } = require('@uniswap/v3-sdk');
+const { ethers } = require('ethers');
+
+const ethereumRpcUrl = "?";
+const chainId = "?";
+const tokenAddress = '?';
+
+if (!ethereumRpcUrl || isNaN(chainId)) {
+  console.error('Veuillez définir les variables d\'environnement YOUR_ETHEREUM_JSON_RPC_URL et CHAIN_ID.');
+  process.exit(1);
 }
+
+async function getSwappableTokens(tokenAddress) {
+  const provider = new ethers.providers.JsonRpcProvider(ethereumRpcUrl); // Utilisez l'URL du nœud Ethereum depuis la variable d'environnement
+
+  try {
+    // Créez un objet Token pour le token dont vous voulez connaître les paires
+    const token = new Token(chainId, tokenAddress, 18); // Remplacez par la précision appropriée
+
+    // Utilisez Fetcher pour obtenir toutes les paires de tokens pour le token spécifié
+    const pairs = await Fetcher.fetchPairsByToken(token, provider);
+
+    // Récupérez la liste des adresses des autres tokens avec lesquels vous pouvez échanger
+    const swappableTokens = pairs.map((pair) => {
+      const otherToken = pair.token0.equals(token) ? pair.token1 : pair.token0;
+      return {
+        address: otherToken.address,
+        symbol: otherToken.symbol,
+      };
+    });
+
+    return swappableTokens;
+  } catch (error) {
+    console.error(`Erreur lors de la récupération des paires pour ${tokenAddress}: ${error.message}`);
+    return [];
+  }
+}
+
+
+
+// TEST
+getSwappableTokens(tokenAddress)
+  .then((swappableTokens) => {
+    console.log(`Tokens échangeables avec ${tokenAddress}:`);
+    swappableTokens.forEach((token) => {
+      console.log(`- ${token.symbol} (Adresse : ${token.address})`);
+    });
+  })
+  .catch(console.error);
