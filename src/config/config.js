ca5e523f3c9df8b87880266b20e05daccb46ecbe
boulinb <37933218+boulinb@users.noreply.github.com>
2023-10-04 23:38:07 +0200

    Configs with decimals

--- src/config/config.js
+++ src/config/config.js
@@ -4,71 +4,88 @@ export default {
         jsonRpc: 'https://rpc.ankr.com/polygon',
         supportedAaveV3Tokens: [{
             abbreviation: "AAVE",
-            contactAddress: "0xd6df932a45c0f255f85145f286ea0b292b21c90b"
+            contactAddress: "0xd6df932a45c0f255f85145f286ea0b292b21c90b",
+            decimals: 18
             },
             {
                 abbreviation: "BAL",
-                contactAddress: "0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3"
+                contactAddress: "0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3",
+                decimals: 18
             },
             {
                 abbreviation: "CRV",
-                contactAddress: "0x172370d5Cd63279eFa6d502DAB29171933a610AF"
+                contactAddress: "0x172370d5Cd63279eFa6d502DAB29171933a610AF",
+                decimals: 18
             },
             {
                 abbreviation: "DAI",
-                contactAddress: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
+                contactAddress: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
+                decimals: 18
             },
             {
                 abbreviation: "DPI",
-                contactAddress: "0x85955046df4668e1dd369d2de9f3aeb98dd2a369"
+                contactAddress: "0x85955046df4668e1dd369d2de9f3aeb98dd2a369",
+                decimals: 18
             },
             {
                 abbreviation: "EURS",
-                contactAddress: "0xe111178a87a3bff0c8d18decba5798827539ae99"
+                contactAddress: "0xe111178a87a3bff0c8d18decba5798827539ae99",
+                decimals: 2
             },
             {
                 abbreviation: "GHST",
-                contactAddress: "0x385eeac5cb85a38a9a07a70c73e0a3271cfb54a7"
+                contactAddress: "0x385eeac5cb85a38a9a07a70c73e0a3271cfb54a7",
+                decimals: 18
             },
             {
                 abbreviation: "LINK",
-                contactAddress: "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39"
+                contactAddress: "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39",
+                decimals: 18
             },
             {
                 abbreviation: "SUSHI",
-                contactAddress: "0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a"
+                contactAddress: "0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a",
+                decimals: 18
             },
             {
                 abbreviation: "USDC",
-                contactAddress: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"
+                contactAddress: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+                decimals: 6
             },
             {
                 abbreviation: "USDT",
-                contactAddress: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
+                contactAddress: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+                decimals: 6
             },
             {
                 abbreviation: "WETH",
-                contactAddress: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"
+                contactAddress: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+                decimals: 18
             },
             {
                 abbreviation: "WBTC",
-                contactAddress: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6"
+                contactAddress: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
+                decimals: 8
             },
             {
                 abbreviation: "WMATIC",
-                contactAddress: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"
+                contactAddress: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+                decimals: 18
             },
             {
                 abbreviation: "agEUR",
-                contactAddress: "0xe0b52e49357fd4daf2c15e02058dce6bc0057db4"
+                contactAddress: "0xe0b52e49357fd4daf2c15e02058dce6bc0057db4",
+                decimals: 18
             },
             {
                 abbreviation: "jEUR",
-                contactAddress: "0x4e3decbb3645551b8a19f0ea1678079fcb33fb4c"
+                contactAddress: "0x4e3decbb3645551b8a19f0ea1678079fcb33fb4c",
+                decimals: 18
             },
             {
                 abbreviation: "miMATIC",
-                contactAddress: "0xa3fa99a148fa48d14ed51d610c367c61876997f1"
+                contactAddress: "0xa3fa99a148fa48d14ed51d610c367c61876997f1",
+                decimals: 18
             }
         ]
     }
