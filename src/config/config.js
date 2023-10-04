52a3ff1aabe2b4d9fe51d78d1ec970bd5a9d2c2b
boulinb <37933218+boulinb@users.noreply.github.com>
2023-10-04 23:12:08 +0200

    Config supported Aave V3 Tokens

---
+++ src/config/config.js
@@ -0,0 +1,56 @@
+export default {
+    polygon: {
+        supportedAaveV3Tokens: [{
+            abbreviation: "AAVE"
+            },
+            {
+                abbreviation: "BAL"
+            },
+            {
+                abbreviation: "CRV"
+            },
+            {
+                abbreviation: "DAI"
+            },
+            {
+                abbreviation: "DPI"
+            },
+            {
+                abbreviation: "EURS"
+            },
+            {
+                abbreviation: "GHST"
+            },
+            {
+                abbreviation: "LINK"
+            },
+            {
+                abbreviation: "SUSHI"
+            },
+            {
+                abbreviation: "USDC"
+            },
+            {
+                abbreviation: "USDT"
+            },
+            {
+                abbreviation: "WETH"
+            },
+            {
+                abbreviation: "WBTC"
+            },
+            {
+                abbreviation: "WMATIC"
+            },
+            {
+                abbreviation: "agEUR"
+            },
+            {
+                abbreviation: "jEUR"
+            },
+            {
+                abbreviation: "miMATIC"
+            }
+        ]
+    }
+}
