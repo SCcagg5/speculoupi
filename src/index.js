6196ec7b6ebabe1aa17f05adfabf3689df4d9346
boulinb <37933218+boulinb@users.noreply.github.com>
2023-10-04 22:49:20 +0200

    Init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import {getRoads} from "./roads";
+import {getPrices} from "./prices";
+
 const main = () => {
     console.log('Hello World!');
+
+    const prices = getPrices();
+
+    const roads = getRoads(prices);
 }
 
 main();
